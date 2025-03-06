52bb4ca6c8ba6f308ca8dd872cccfcedeeb3dc57
Fredrik Kis <fredrik.kis03@gmail.com>
2025-03-06 20:36:01 +0100

    Basic CMakeList added, for src file and linking Eigen. Main file only basic outline of program.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+
+int main(int argc, char** argv)
+{
+  // Parse input arguemnts?
+
+  // Define/Load camera calibration parameters
+
+  // Create a UDP or file reader for event data. Multi threaded?
+
+  // Initialize the DSI
+
+  // Read events, user buffers?
+
+
+
+}
