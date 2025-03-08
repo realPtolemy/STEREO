8ab2a15acfa1605a916aa97965a3f7b6b36c2321
Fredrik Kis <fredrik.kis03@gmail.com>
2025-03-08 10:48:08 +0100

    CMakeLists done for now

--- src/main.cpp
+++ src/main.cpp
@@ -32,11 +32,4 @@ int main(int argc, char** argv)
 
   // Read events, user buffers?
 
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
-
 }
